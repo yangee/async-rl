5e45a70a4d84a23d961f7ba212e4b879a827c38b
Ji Hyun Yang <sodapop92@gmail.com>
2017-01-13 18:22:14 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,9 @@ from keras.layers import Convolution2D, Flatten, Dense, Input
 from keras.models import Model
 
 def build_network(num_actions, agent_history_length, resized_width, resized_height):
-  with tf.device("/cpu:0"):
+  with tf.device("/cpu:0"): #server CPU
+    # placeholder for tensor 
+    # None - unlimited # of rows
     state = tf.placeholder("float", [None, agent_history_length, resized_width, resized_height])
     inputs = Input(shape=(agent_history_length, resized_width, resized_height,))
     model = Convolution2D(nb_filter=16, nb_row=8, nb_col=8, subsample=(4,4), activation='relu', border_mode='same')(inputs)
